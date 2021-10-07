815336f0727c82d629a552a054acde17c400ce95
Jay shah <shah.jay60@yahoo.com>
2021-10-07 22:17:03 +0530

    Commit code

---
+++ HpAccountant/Controllers/ServiceController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HpAccountant.Controllers
+{
+    public class ServiceController : Controller
+    {
+        // GET: Service
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult YearEndAccounts()
+        {
+            return View();
+        }
+        public ActionResult Bookkeeping()
+        {
+            return View();
+        }
+        public ActionResult Payroll()
+        {
+            return View();
+        }
+        public ActionResult SelfAssessment()
+        {
+            return View();
+        }
+        public ActionResult ManagementAccounting()
+        {
+            return View();
+        }
+    }
+}
